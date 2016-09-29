5b68c8bf97aa274c26d6cd3f74b289dfdbf46a50
Alexander Schmidt <alexanderschmidt1@gmail.com>
2016-09-29 13:43:09 +0200

    Create bootstrap container for content

--- src/components/App.js
+++ src/components/App.js
@@ -12,12 +12,11 @@ class App extends Component {
             <div className='App'>
                 <div className='App-header'>
                     <img src={logo} className='App-logo' alt='logo' />
-                    <h2>Welcome to React</h2>
+                    <h2>Demo page for testing redux-form</h2>
                 </div>
-                <p className='App-intro'>
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
-                <MyForm />
+                <div className='container'>
+                    <MyForm />
+                </div>
             </div>
         );
     }
