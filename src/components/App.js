f70e27f4b8a296f793479f590109b97a91ac4858
Alexander Schmidt <alexanderschmidt1@gmail.com>
2016-09-30 10:52:48 +0200

    Improve styling (spaces)

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,7 @@ class App extends Component {
             <div className='App'>
                 <div className='App-header'>
                     <img src={logo} className='App-logo' alt='logo' />
-                    <h2>Demo page for testing redux-form</h2>
+                    <h2>Testing redux-form</h2>
                 </div>
                 <div className='container'>
                     <MyForm />
